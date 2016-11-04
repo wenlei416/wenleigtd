d22c335b93ee64966813b33b35dc62bd3193d294
wenlei416 <leiwen.cn@gmail.com>
2016-11-04 21:50:04 +0800

    注释所有outlook相关代码

--- GTD.Web/Util/OutlookAppointment.cs
+++ GTD.Web/Util/OutlookAppointment.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Microsoft.Office.Interop.Outlook;
+//using Microsoft.Office.Interop.Outlook;
 
 namespace GTD.Util
 {
@@ -9,19 +9,19 @@ namespace GTD.Util
         {
             //这个方法需要本地有outlook，引入com  microsoft.office.interop.outlook 16版本
             //from http://www.codeproject.com/Tips/84321/Setting-up-an-MS-Outlook-Appointment-C
-            Application outlookApplication = new Application();
-            AppointmentItem oAppointment = (AppointmentItem)outlookApplication.CreateItem(OlItemType.olAppointmentItem);
-            oAppointment.Subject = "约会标题"; // set the subject
-            oAppointment.Body = "约会内容"; // set the body
-            oAppointment.Location = "任意地点"; // set the location
-            oAppointment.Start = Convert.ToDateTime(DateTime.Now); // Set the start date 
-            oAppointment.End = Convert.ToDateTime(DateTime.Now.AddMinutes(2)); // End date 
-            oAppointment.ReminderSet = true; // Set the reminder
-            oAppointment.ReminderMinutesBeforeStart = 5; // reminder time
-            oAppointment.Importance = OlImportance.olImportanceHigh; // appointment importance
-            oAppointment.BusyStatus = OlBusyStatus.olBusy;
-            oAppointment.AllDayEvent = false;
-            oAppointment.Save();
+            //Application outlookApplication = new Application();
+            //AppointmentItem oAppointment = (AppointmentItem)outlookApplication.CreateItem(OlItemType.olAppointmentItem);
+            //oAppointment.Subject = "约会标题"; // set the subject
+            //oAppointment.Body = "约会内容"; // set the body
+            //oAppointment.Location = "任意地点"; // set the location
+            //oAppointment.Start = Convert.ToDateTime(DateTime.Now); // Set the start date 
+            //oAppointment.End = Convert.ToDateTime(DateTime.Now.AddMinutes(2)); // End date 
+            //oAppointment.ReminderSet = true; // Set the reminder
+            //oAppointment.ReminderMinutesBeforeStart = 5; // reminder time
+            //oAppointment.Importance = OlImportance.olImportanceHigh; // appointment importance
+            //oAppointment.BusyStatus = OlBusyStatus.olBusy;
+            //oAppointment.AllDayEvent = false;
+            //oAppointment.Save();
             //到这里已经保存到本地outlook中了
 
 
