013b4f18adb13c423b32b4984df8c12961f133bc
wenlei416 <leiwen.cn@gmail.com>
2016-11-17 23:16:34 +0800

    完成循环日算法的年周日部分（少UT）

---
+++ GTD.UT/Util/RecurringDateTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using GTD.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTD.UT.Util.Tests
+{
+    [TestClass()]
+    public class RecurringDateTests
+    {
+        //全部循环日期都在范围内
+        [TestMethod()]
+        public void RecurringJsonToDateYear1Test()
+        {
+            const string cyc = @"{'cyear':'1','cmonth':'0','cweek':'0','cday':'0','startday':'2016-1-2','endday':'2018-4-5','cyc':'12'}";
+            var a = RecurringDate.RecurringJsonToDate(cyc);
+
+            Assert.AreEqual(new DateTime(2016, 1, 12), a[0]);
+            Assert.AreEqual(new DateTime(2017, 1, 12), a[1]);
+            Assert.AreEqual(new DateTime(2018, 1, 12), a[2]);
+        }
+
+        //闰年
+        [TestMethod]
+        public void RecurringJsonToDateYear2Test()
+        {
+            const string cyc = @"{'cyear':'1','cmonth':'0','cweek':'0','cday':'0','startday':'2016-1-2','endday':'2024-4-5','cyc':'366'}";
+            var a = RecurringDate.RecurringJsonToDate(cyc);
+
+            Assert.AreEqual(new DateTime(2016, 2, 29), a[0]);
+            Assert.AreEqual(new DateTime(2017, 2, 28), a[1]);
+            Assert.AreEqual(new DateTime(2018, 2, 28), a[2]);
+            Assert.AreEqual(new DateTime(2019, 2, 28), a[3]);
+            Assert.AreEqual(new DateTime(2020, 2, 28), a[4]);
+            //Assert.AreEqual(new DateTime(2020, 2, 29), a[8]);
+        }
+        //所有循环日期都不在范围内
+        [TestMethod]
+        public void RecurringJsonToDateYear3Test()
+        {
+            const string cyc = @"{'cyear':'1','cmonth':'0','cweek':'0','cday':'0','startday':'2016-10-2','endday':'2016-12-15','cyc':'20'}";
+            var a = RecurringDate.RecurringJsonToDate(cyc);
+
+            Assert.AreEqual(a.Count,0);
+        }
+
+        //头尾不在
+        [TestMethod]
+        public void RecurringJsonToDateYear4Test()
+        {
+            const string cyc = @"{'cyear':'1','cmonth':'0','cweek':'0','cday':'0','startday':'2016-10-1','endday':'2019-2-1','cyc':'100'}";
+            var a = RecurringDate.RecurringJsonToDate(cyc);
+
+            Assert.AreEqual(new DateTime(2017, 4, 10), a[0]);
+            Assert.AreEqual(new DateTime(2018, 4, 10), a[1]);
+        }
+    }
+}
---
+++ GTD.Web/Util/RecurringDate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace GTD.Util
+{
+    public static class RecurringDate
+    {
+        public static List<DateTime> RecurringJsonToDate(string recurringJson)
+        {
+            //返回值
+            List<DateTime> recurringDateTimes = new List<DateTime>();
+
+            //把json字符串解析为dic，并把对应的值取出来
+            Dictionary<string, string> recurringDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(recurringJson);
+            int cyear = Convert.ToInt32(recurringDictionary["cyear"]);
+            int cmonth = Convert.ToInt32(recurringDictionary["cmonth"]);
+            int cweek = Convert.ToInt32(recurringDictionary["cweek"]);
+            int cday = Convert.ToInt32(recurringDictionary["cday"]);
+
+            //循环日，表示和按json中表明的循环方法，离这个循环方法的第一天距离多少天
+            int cyc = Convert.ToInt32(recurringDictionary["cyc"]);
+
+            DateTime startDateTime = Convert.ToDateTime(recurringDictionary["startday"]);
+            DateTime endday = Convert.ToDateTime(recurringDictionary["endday"]);
+
+            //每个可能的循环日
+            DateTime cycDateTime;
+            //year不等于0，表示按年循环
+            if (cyear != 0)
+            {
+                int y = startDateTime.Year;
+                //业务规则，cyc设为366，表示循环2-29
+                if (cyc == 366)
+                {
+                    //构造第一个循环日
+                    cycDateTime = new DateTime(startDateTime.Year, 2, 29);
+                }
+                else
+                {
+                    //非闰年，则随便找个普通年，计算那年的第cyc天是几月几号
+                    DateTime dtBase = new DateTime(2001, 1, 1);
+                    DateTime dtDate = dtBase.AddDays(cyc - 1);
+                    //构造第一个循环日
+                    cycDateTime =
+                        new DateTime(startDateTime.Year, dtDate.Month, dtDate.Day);
+                }
+                //对2-29不好使
+                //如果循环日小于结束日，进循环
+                while (cycDateTime <= endday)
+                {
+                    //如果循环日小于开始日期，不能加入返回队列，直接年加1
+                    if (cycDateTime < startDateTime)
+                    {
+                        cycDateTime = cycDateTime.AddYears(cyear);
+                    }
+                    //如果循环日大于开始日期，加入返回队列，年加1
+                    else
+                    {
+                        recurringDateTimes.Add(cycDateTime);
+                        cycDateTime = cycDateTime.AddYears(cyear);
+                    }
+                }
+            }
+            else if (cmonth != 0)
+            {
+
+            }
+            else if (cweek != 0)
+            {
+                //周日是0
+                cycDateTime = startDateTime.AddDays(cyc - (int)startDateTime.DayOfWeek);
+                while (cycDateTime <= endday)
+                {
+                    //如果循环日小于开始日期，不能加入返回队列，直接日加7
+                    if (cycDateTime < startDateTime)
+                    {
+                        cycDateTime = cycDateTime.AddDays(cweek * 7);
+                    }
+                    //如果循环日大于开始日期，加入返回队列，日加7
+                    else
+                    {
+                        recurringDateTimes.Add(cycDateTime);
+                        cycDateTime = cycDateTime.AddDays(cweek * 7);
+                    }
+                }
+            }
+            else if (cday != 0)
+            {
+                cycDateTime = startDateTime;
+                while (cycDateTime <= endday)
+                {
+                    recurringDateTimes.Add(cycDateTime);
+                    cycDateTime = cycDateTime.AddDays(cday);
+                }
+            }
+            return recurringDateTimes;
+
+        }
+    }
+}
