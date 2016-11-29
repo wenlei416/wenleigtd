64e86dfba7ca4ead8108c71b8b1e0afc3e04e057
wenlei416 <leiwen.cn@gmail.com>
2016-11-29 18:23:06 +0800

    尝试log，比较麻烦，暂停

--- GTD.Web/Services/TaskServices.cs
+++ GTD.Web/Services/TaskServices.cs
@@ -218,7 +218,7 @@ namespace GTD.Services
             _taskRepository.BatchUpdateTask(tasks);
         }
 
-        [Log]
+        //[Log]
         public Task GetTaskById(int? taskId)
         {
             return _taskRepository.GetTaskById(taskId);
--- GTD.Web/Util/LogInterceptor.cs
+++ GTD.Web/Util/LogInterceptor.cs
@@ -2,16 +2,15 @@
 
 namespace GTD.Util
 {
-    public class LogInterceptor: SimpleInterceptor
+    public class LogInterceptor : SimpleInterceptor
     {
         protected override void BeforeInvoke(IInvocation invocation)
         {
-            //LogHelper.WriteLog($"BeforeInvoke : Method: {invocation.Request.Method.Name} called with arguments {invocation.Request.Arguments} returned {invocation.ReturnValue}");
+            LogHelper.WriteLog($"BeforeInvoke : Method: {invocation.Request.Method.Name} Arguments {invocation.Request.Arguments[0]} returned {invocation.ReturnValue}");
         }
 
         protected override void AfterInvoke(IInvocation invocation)
         {
-
             LogHelper.WriteLog($"AfterInvoke  : Method: {invocation.Request.Method.Name} Arguments: {invocation.Request.Arguments[0]} Returned: {invocation.ReturnValue}");
         }
     }
